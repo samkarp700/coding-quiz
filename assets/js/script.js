ad52b916343b2526464ddcb28c671d28a9a9c9dd
Sam Karp <sfcypret@gmail.com>
2022-07-09 18:24:00 -0400

    function layout with pseudo

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// global variables for quiz page
+var timerEl = document.getElementById("time-left");
+var greetingEl = document.getElementById("greet");
+var startEl = document.getElementById("start-btn");
+var quizEl = document.getElementById("quiz-questions");
+var questionEl = document.getElementById("question");
+var answerEl = doucment.getElementById("answer-buttons");
+
+//timer start settings
+var startTime = 60;
+
+//functions main
+
+var startGame = function() {
+    //start button event listener
+    //hide greeting
+    //start question loop
+}
+
+var startTime = function () {
+    //timer begins
+}
+
+var setQuestion = function () {
+    //new question and answer choices
+}
+
+var answerStatus = function () {
+    //check answer
+}
+
+var endGame = function () {
+    //timer runs out or last question
+    //prompt to save high score
+}
+
+var saveHighScore = function () {
+    // set initials and score to local storage
+}
+
+//question and answer array for question loop
+
