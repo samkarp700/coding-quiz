0311659f8a9509a9a0c623ce28e8a70d5c39c323
Sam Karp <sfcypret@gmail.com>
2022-07-10 19:41:23 -0400

    scores saving to local storage but not showing on page

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,2 +1,19 @@
 //high score page only 
 
+var showScoreList = document.getElementById('show-scores');
+
+
+// pull from local storage and display players and scores
+
+function showHighScore() {
+   window.localStorage.getItem('nameScoreArr')
+   var highScores = JSON.parse(window.localStorage.getItem('nameScoreArr'));
+   
+   highScores.forEach(function(showScoreItems) {
+   var showScoreItems = document.createElement('ul');
+   showScoreItems.textContent = "Player: " + playerEl + "Score: " + scoreEl;
+   showScoreList.appendChild(showScoreItems)
+})
+
+};
+
